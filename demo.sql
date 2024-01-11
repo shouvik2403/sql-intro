3836369271283fb9b7932b7fce33e9326149a463
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-01-11 01:37:33 +0000

    Class 2: 1st half

--- demo.sql
+++ demo.sql
@@ -1,3 +1,11 @@
 -- Get the first and last name of every player in the database.
 
 SELECT first_name, last_name FROM players;
+
+-- USEFUL COMMANDS
+-- sqlite3 baseball.sqlite3
+-- .schema
+-- .mode columns
+-- .headers on
+-- .quit
+-- .read <filename.sql>

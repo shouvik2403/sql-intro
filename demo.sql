cbfd93e7da3d4a5b0e6fc34dafd9d256db1fd362
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-01-11 02:38:02 +0000

    Class 2: Lab 3

--- demo.sql
+++ demo.sql
@@ -2,10 +2,26 @@
 
 SELECT first_name, last_name FROM players;
 
--- USEFUL COMMANDS
--- sqlite3 baseball.sqlite3
--- .schema
--- .mode columns
--- .headers on
--- .quit
--- .read <filename.sql>
+-- GENERAL USEFUL COMMANDS
+sqlite3 baseball.sqlite3 -- load sqlite environment with baseball.sqlite3 database
+.schema --schema of database columns headers
+.mode columns --formatting
+.headers on --formatting
+.quit --exit sqlite environment
+.read <filename.sql> --execute code in .sql file
+; --something goes wrong, go back to sqlite> prompt 
+ 
+-- LAB 2 general syntax
+SELECT ____ FROM _____
+WHERE _______
+AND _______
+ORDER BY _______
+LIMIT ______;
+
+-- LAB 2 general syntax
+SELECT ____, COUNT(*) / AVG(column) / MAX(column) / MIN (column) / SUM (column) FROM _____
+WHERE _______
+AND _______
+GROUP BY _______
+ORDER BY _______
+LIMIT ______;
